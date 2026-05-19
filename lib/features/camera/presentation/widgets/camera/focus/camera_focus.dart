e5344e833c7a174d3eee6b6c4b374c1bbad05aa7
ojas-mecha <ojasc@ext.mechasystems.com>
2026-05-19 10:58:31 +0530

    fix: expsure line duplication

--- lib/features/camera/presentation/widgets/camera/focus/camera_focus.dart
+++ lib/features/camera/presentation/widgets/camera/focus/camera_focus.dart
@@ -61,7 +61,6 @@ class _CameraFocusState extends State<CameraFocus>
               ),
             ),
           ),
-          const BrightnessLine(),
           if (!isStart) const BrightnessLine(),
         ],
       ),
